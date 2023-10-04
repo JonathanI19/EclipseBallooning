470c08c89adcb23e6e91be83a0ff219ad48c84bb
JonathanI19 <jmisely19@gmail.com>
2023-10-04 16:18:08 -0400

    Rudimentary code finished for camera processing - Needs cleaning up

--- rPi/sun_tracker/main.py
+++ rPi/sun_tracker/main.py
@@ -35,6 +35,8 @@ def main():
             # output the frame
             out.write(frame) 
             
+            # The original input frame is shown in the window 
+            cv2.imshow('Original', frame)
             if(frame_count == (fps/samples_per_second)):
                 frame_count = 0
                 cProc.set_frame(frame)
@@ -45,8 +47,28 @@ def main():
                 cv2.imshow('q2', q2)
                 cv2.imshow('q3', q3)
                 
-            # The original input frame is shown in the window 
-            cv2.imshow('Original', frame)
+                # Getting brightness values
+                v0, v1, v2, v3 = cProc.get_brightness()
+
+                # converting to list and getting index of brightest
+                values = [v0,v1,v2,v3]
+                quadrants = [q0,q1,q2,q3]
+                brightest = np.argmax(values)
+
+                # Converting brightest back to original color space
+                quadrants[brightest] = cv2.cvtColor(quadrants[brightest], cv2.COLOR_HSV2RGB)
+
+                
+                # Example of recombining frame
+                half_w = frame_width//2
+                half_h = frame_height//2
+                new_frame = frame
+                new_frame[:half_h, half_w:] = quadrants[0]
+                new_frame[:half_h, :half_w] = quadrants[1]
+                new_frame[half_h:, :half_w] = quadrants[2]
+                new_frame[half_h:, half_w:] = quadrants[3]
+                cv2.imshow("New Frame", new_frame)
+
         
             # Wait for 'a' key to stop the program 
             if cv2.waitKey(1) & 0xFF == ord('a'):
--- rPi/sun_tracker/util/camera_processor.py
+++ rPi/sun_tracker/util/camera_processor.py
@@ -1,4 +1,6 @@
 import cv2
+from statistics import mean
+import numpy as np
 
 class Camera_Processor:
     def __init__(self, size):
@@ -10,12 +12,13 @@ class Camera_Processor:
         self.w, self.h = size
         self.half_w = self.w//2
         self.half_h = self.h//2
+        self.pxl = self.half_w*self.half_h
 
     def set_frame(self, frame):
         self.__frame = frame
 
     def convert_frame(self):
-        self.__frame = cv2.cvtColor(self.__frame, cv2.COLOR_BGR2HSV)
+        self.__frame = cv2.cvtColor(self.__frame, cv2.COLOR_RGB2HSV)
 
     def split_frame(self):
         self.__q0 = self.__frame[:self.half_h, self.half_w:]
@@ -25,5 +28,17 @@ class Camera_Processor:
         return(self.__q0, self.__q1, self.__q2, self.__q3)
 
 
-    #def get_brightness(self):
-        
+    def get_brightness(self):
+        v0 = np.sum(self.__q0[:,:,2])
+        avg_v0 = v0 / self.pxl
+
+        v1 = np.sum(self.__q1[:,:,2])
+        avg_v1 = v1 / self.pxl
+
+        v2 = np.sum(self.__q2[:,:,2])
+        avg_v2 = v2 / self.pxl
+
+        v3 = np.sum(self.__q3[:,:,2])
+        avg_v3 = v3 / self.pxl
+
+        return(avg_v0, avg_v1, avg_v2, avg_v3)
